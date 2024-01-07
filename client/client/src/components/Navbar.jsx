e361a11498841c22ee1a394186487fa37af53018
Aayush Joshi <aayushj2001@gmail.com>
2024-01-07 21:54:11 +0545

    feat: lil bit of twig

--- client/client/src/components/Navbar.jsx
+++ client/client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 //react icons
-import { FaBlog } from "react-icons/fa6";
+import { FaBarsStaggered, FaBlog } from "react-icons/fa6";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -41,9 +41,31 @@ const Navbar = () => {
     <header>
       <nav>
         {/*logo*/}
-        <link to="/">
-          <FaBlog />
+        <link
+          to="/"
+          className="text-2xl font-bold text-blue-700 flex-items-center gap-2"
+        >
+          <FaBlog className="inline-block" />
         </link>
+        {/* nav items for large device */}
+        <ul className="md: flex' space-x-12 hidden">
+          {navItem.map(({ link, path }) => {
+            <Link
+              key={path}
+              to={path}
+              className="block text-base text-black uppercase cursor-pointer hover:text-blue-700"
+            >
+              {link}
+            </Link>;
+          })}
+        </ul>
+
+        {/* btn for lg devices */}
+        <div className="space-x-12 hidden lg:flex items-center">
+          <button>
+            <FaBarsStaggered className="w-5 hover:text-blue-700" />
+          </button>
+        </div>
       </nav>
     </header>
   );
